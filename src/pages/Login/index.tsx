dd5f725f1ea67197914f57a46a8c151b2b9a21fc
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-07-02 18:10:19 -0700

    added login authentication

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -13,8 +13,61 @@ import Divider from "@mui/joy/Divider";
 import '../../../styles/googleSignIn.css'
 import { AuthType } from "@/hook/Auth";
 import { withPublic } from "@/hook/Routes";
+import { useEffect, useState } from "react";
+import FormHelperText from "@mui/joy/FormHelperText";
 
 function App({auth}: {auth: AuthType}) {
+
+  const [email, setEmail] = useState('');
+  const [emailErr, setEmailErr] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailErr('');
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordErr('');
+    setPassword(event.target.value);
+  };
+
+  const checkError = () => {
+    const err = auth?.error;
+    console.log(err);
+
+    if (err === undefined) return;
+
+    if (err.includes('(auth/invalid-email)')) {
+      setEmailErr('Invalid Email');
+    }
+
+    if (err.includes('auth/auth/user-disabled')) {
+      setEmailErr('User is currently disabled');
+    }
+
+    if (err.includes('auth/user-not-found')) {
+      setEmailErr('User not found');
+    }
+
+    if (err.includes('auth/wrong-password')) {
+      setPasswordErr('incorrect password');
+    }
+
+    if (err.includes('auth/network-request-failed')) {
+      setEmailErr('Network Connection Error. Please Try again later.')
+    }
+
+    if (err.includes('auth/too-many-requests')) {
+      setEmailErr('Too many requests');
+    }
+  }
+
+  useEffect(() => {
+    checkError();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [auth?.error])
 
   return (
     <CssVarsProvider>
@@ -37,17 +90,26 @@ function App({auth}: {auth: AuthType}) {
           <Typography textColor="gray" level="body1" textAlign="center">We&apos;ll check if you have an email</Typography>
         </div>
 
-        <FormControl>
+        <FormControl required>
           <FormLabel>Email</FormLabel>
           <Input
             name="email"
             type="email"
             placeholder="johndoe@email.com"
+            value={email}
+            error={emailErr!==''}
+            onChange={handleEmailChange}
           />
+          <FormHelperText>
+            {emailErr !== '' && emailErr}
+          </FormHelperText>
         </FormControl>
-        <FormControl>
+        <FormControl required>
           <FormLabel>Password</FormLabel>
-          <Input name="password" type="password" placeholder="password" />
+          <Input name="password" type="password" placeholder="password" value={password} onChange={handlePasswordChange} error={passwordErr!==''} />
+          <FormHelperText>
+            {passwordErr !== '' && passwordErr}
+          </FormHelperText>
         </FormControl>
 
         <Divider>
@@ -58,7 +120,7 @@ function App({auth}: {auth: AuthType}) {
           Sign in with Google
         </button>
 
-        <Button onClick={function (){}} style={{marginTop: 20}} variant="solid">
+        <Button onClick={async () => {await auth?.loginWithEmailAndPassword(email, password).then(() => {checkError()})}} style={{marginTop: 20}} variant="solid">
           Continue
         </Button>
         <Typography
