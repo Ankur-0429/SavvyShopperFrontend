5b59db360d5418538e5b85339d0c1e07286aace7
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-07-07 22:26:52 -0700

    small fix

--- src/components/Model/index.tsx
+++ src/components/Model/index.tsx
@@ -40,6 +40,7 @@ export default function Model() {
     }, 1000);
 
     return () => clearTimeout(delayDebounceFn);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [url]);
 
   return (
