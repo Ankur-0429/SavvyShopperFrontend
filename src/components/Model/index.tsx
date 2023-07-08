010b22e51b4edcbd838d3de8d4c2be8c4e493ee7
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-07-07 20:35:35 -0700

    small cleanup

--- src/components/Model/index.tsx
+++ src/components/Model/index.tsx
@@ -24,48 +24,42 @@ export default function Model() {
         Track New Item
       </Button>
       <Typography>
-      <Modal
-        blur
-        aria-labelledby="modal-title"
-        open={visible}
-        onClose={closeHandler}>
-        <Modal.Header>
-          
-            <Typography level="h5">
-              Track new item
+        <Modal
+          blur
+          aria-labelledby="modal-title"
+          open={visible}
+          onClose={closeHandler}>
+          <Modal.Header>
+            <Typography level="h5">Track new item</Typography>
+          </Modal.Header>
+          <Modal.Body>
+            <Typography>
+              <Input
+                bordered
+                fullWidth
+                size="lg"
+                placeholder="Paste URL here"
+                value={url}
+                color={urlErr !== "" ? "error" : "primary"}
+                onChange={(event) => {
+                  setUrlErr("");
+                  setUrl(event.target.value);
+                }}
+                helperText={urlErr}
+                helperColor="error"
+              />
             </Typography>
-        </Modal.Header>
-        <Modal.Body>
-          <Typography>
-            <Input
-              bordered
-              fullWidth
-              size="lg"
-              placeholder="Paste URL to track your item"
-              value={url}
-              color={urlErr !== "" ? "error" : "primary"}
-              onChange={(event) => {
-                setUrlErr("");
-                setUrl(event.target.value);
-              }}
-              helperText={urlErr}
-              helperColor="error"
-            />
-          </Typography>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button auto flat color="error" onPress={closeHandler}>
-            Close
-          </Button>
-          <Button auto onPress={closeHandler} disabled>
-            Add Task
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
+          </Modal.Body>
+          <Modal.Footer>
+            <Button auto flat color="error" onPress={closeHandler}>
+              Close
+            </Button>
+            <Button auto onPress={closeHandler} disabled>
+              Add Task
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Typography>
-
-      
     </div>
   );
 }
