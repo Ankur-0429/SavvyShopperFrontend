2b68f1490216c57c3e2a8ecf4589e9d3299cf69d
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-07-12 10:58:45 -0700

    check if google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+          {/* ...other head elements */}
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_TRACKING_ID" />
+          <Script
+            id="my-script"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'YOUR_TRACKING_ID', { page_path: window.location.pathname });
+              `,
+            }}
+          />
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,71 +5,51 @@ import "../app/globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../../firebase/clientApp";
-import { useRouter } from "next/router";
+import { initGA, logPageView } from "@/service/ga";
 import { useEffect } from "react";
-import Script from "next/script";
+import { useRouter } from "next/router";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      window.gtag(
-        "config",
-        process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
-        {
-          page_path: url,
-        }
-      );
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-    handleRouteChange(window.location.pathname);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    initGA();
+    logPageView();
   }, []);
 
+  useEffect(() => {
+    const handleRouteChange = (url:string) => {
+      logPageView();
+    };
+
+    // Event listener for route changes
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
   return (
-    <>
-      {/* gtag script code */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string}', {
-            page_path: window.location.pathname
-          });`,
-        }}
-      />
-      <AuthProvider>
-        <AuthStateChanged>
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-          <Component {...pageProps} />
-        </AuthStateChanged>
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <AuthStateChanged>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <Component {...pageProps} />
+      </AuthStateChanged>
+    </AuthProvider>
   );
 };
 
