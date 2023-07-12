5f9b7fab152df034710a2c884b3b94445207cdc2
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-07-12 11:05:40 -0700

    check if google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,19 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-          {/* ...other head elements */}
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=YOUR_TRACKING_ID" />
-          <Script
-            id="my-script"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'YOUR_TRACKING_ID', { page_path: window.location.pathname });
-              `,
-            }}
-          />
       <body className={inter.className}>{children}</body>
     </html>
   )
---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}', { page_path: window.location.pathname });
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
