52a1dbd2dffebc2a4f8c0846cd7321d99bd429a2
Ankur Ahir <ahir.ankur0429@gmail.com>
2023-07-19 11:08:13 -0700

    small change

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -1,70 +1,69 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import Chart, { ChartItem } from "chart.js/auto";
+import Chart from "chart.js/auto";
 
 interface LineChartType {
-    prices: number[];
-    start_date: string;
+  prices: number[];
+  start_date: string;
 }
 
-const isoStringToDate = (isoString:string) => {
-    const date = new Date(isoString);
-    const options = { month: 'long', day: 'numeric' };
-    // @ts-ignore
-    const formattedDate = date.toLocaleDateString('en-US', options);
-    const day = date.getDate();
-    const suffix = getDaySuffix(day);
-    return `${formattedDate}${suffix}`;
-  };
-  
-  const getDaySuffix = (day:number) => {
-    if (day >= 11 && day <= 13) {
-      return 'th';
-    }
-    switch (day % 10) {
-      case 1:
-        return 'st';
-      case 2:
-        return 'nd';
-      case 3:
-        return 'rd';
-      default:
-        return 'th';
-    }
-  };
+const isoStringToDate = (isoString: string) => {
+  const date = new Date(isoString);
+  const options = { month: "long", day: "numeric" };
+  // @ts-ignore
+  const formattedDate = date.toLocaleDateString("en-US", options);
+  const day = date.getDate();
+  const suffix = getDaySuffix(day);
+  return `${formattedDate}${suffix}`;
+};
 
-  const getFormattedDate = (date: Date) => {
-    const options = { month: 'long', day: 'numeric' };
-    // @ts-ignore
-    return date.toLocaleDateString('en-US', options);
-  };
+const getDaySuffix = (day: number) => {
+  if (day >= 11 && day <= 13) {
+    return "th";
+  }
+  switch (day % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+};
 
-  const generateDateStringLoop = (n:number, s: string) => {
-    const startDate = new Date(s);
-    const dateStrings = [];
-  
-    for (let i = 0; i < n; i++) {
-      const currentDate = new Date(startDate.getTime() + i * 24 * 60 * 60 * 1000); // Add i days to the starting date
-      const formattedDate = getFormattedDate(currentDate);
-      dateStrings.push(isoStringToDate(formattedDate));
-    }
-  
-    return dateStrings;
-  };
-  
+const getFormattedDate = (date: Date) => {
+  const options = { month: "long", day: "numeric" };
+  // @ts-ignore
+  return date.toLocaleDateString("en-US", options);
+};
+
+const generateDateStringLoop = (n: number, s: string) => {
+  const startDate = new Date(s);
+  const dateStrings = [];
+
+  for (let i = 0; i < n; i++) {
+    const currentDate = new Date(startDate.getTime() + i * 24 * 60 * 60 * 1000); // Add i days to the starting date
+    const formattedDate = getFormattedDate(currentDate);
+    dateStrings.push(isoStringToDate(formattedDate));
+  }
+
+  return dateStrings;
+};
 
 function isValidISOString(dateString: string): boolean {
-    return new Date(dateString).toISOString() === dateString;
+  return new Date(dateString).toISOString() === dateString;
 }
 
-const Linechart = ({prices, start_date}:LineChartType) => {
+const Linechart = ({ prices, start_date }: LineChartType) => {
   const chartRef = useRef<HTMLCanvasElement>(null);
   const chartInstance = useRef<Chart | null>(null);
 
   useEffect(() => {
     if (!isValidISOString(start_date)) {
-        return () => null;
+      return () => null;
     }
 
     const isoStrings = generateDateStringLoop(prices.length, start_date);
@@ -86,6 +85,21 @@ const Linechart = ({prices, start_date}:LineChartType) => {
         chartInstance.current = new Chart(ctx, {
           type: "line",
           data: data,
+          options: {
+            hover: {
+              intersect: false,
+              mode: "index",
+            },
+            elements: {
+              line: {
+                borderWidth: 2,
+                hoverBorderWidth: 5,
+              },
+              point: {
+                hoverRadius: 8,
+              },
+            },
+          },
         });
       }
     }
